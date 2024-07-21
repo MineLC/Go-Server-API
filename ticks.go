a3ce79f52fce11f62e5c3ac2ea06e85be791ec0d
Mona Lisa <ichocodev@gmail.com>
2024-07-21 18:12:18 -0300

    Fix package names

--- ticks.go
+++ ticks.go
@@ -1,4 +1,4 @@
-package main
+package api
 
 type Mspt interface {
 	GetPromedium() float32
--- util/masking.go
+++ util/masking.go
@@ -1,12 +1,10 @@
 package mask
 
-type Masking struct{}
-
-func (m *Masking) Has(mask, field byte) bool {
+func Has(mask, field byte) bool {
 	return mask&field != 0
 }
 
-func (m *Masking) Set(mask *byte, field byte, when bool) {
+func Set(mask *byte, field byte, when bool) {
 	if when {
 		*mask |= field
 	}
